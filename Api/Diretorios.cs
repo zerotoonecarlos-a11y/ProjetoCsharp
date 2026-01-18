06903ace69ef5f79e34751c3ee2d80cc00d88d19
carlos <geraldocarlos503@gmail.com>
2026-01-18 09:17:42 -0300

    Trabalhando com chamadas do sistema

--- Api/Diretorios.cs
+++ Api/Diretorios.cs
@@ -8,5 +8,22 @@ public class Diretorios
         var novoDirDestino = @"~/PastaCharpDestino".ParseHome();
         var dirProjeto = @"~/source/repos/cursoCSharp/cursoCSharp".ParseHome();
 
+        if(Directory.Exists(novoDir)) Directory.Delete(novoDir, true);
+
+        if(Directory.Exists(novoDirDestino)) Directory.Delete(novoDirDestino, true);
+
+        Directory.CreateDirectory(novoDir);
+        Console.WriteLine(Directory.GetCreationTime(novoDir));
+
+
+        Console.WriteLine("===Pastas===========");
+        var pastas = Directory.GetDirectories(dirProjeto);
+
+        foreach(var pasta in pastas) Console.WriteLine(pasta);
+        
+
+        var arquivos = Directory.GetFiles(dirProjeto);
+
+        foreach(var arquivo in arquivos) Console.Write(arquivo);
     }
 }
